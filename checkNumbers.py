0ac77d45927fb14f16aa930056bd00289f9d83f3
A-J-007 <alivelu.j1@gmail.com>
2024-12-13 04:35:01 +0530

    using if else condition

---
+++ checkNumbers.py
@@ -0,0 +1,9 @@
+# This program is to check the given number is positive or negative
+
+number = int(input("give a number: "))            
+if number < 0:
+    print("The given number is negative number")
+elif number == 0:
+     print("The given number is zero")
+else:
+     print("The given number is positive number")  
